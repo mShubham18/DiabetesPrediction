eb1c9a79860802b3d17dcdd11127e2af20fd09d5
mShubham18 <shubhuu1804@gmail.com>
2025-02-14 19:05:31 +0530

    Done with the Structure and functioning of the Website, proceeding to the styling procedure

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,url_for
 import numpy as np
 import pickle
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
-scaler=StandardScaler()
 with open("Resources/model.pkl","rb") as file:
         model = pickle.load(file)
 
-
+with open("Resources/scaler.pkl","rb") as file:
+        scaler = pickle.load(file)
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def main():
-    return render_template("form.html")
+    return render_template("form.html", prediction=None)
 
 @app.route("/submit",methods=["POST"])
 def submit():
@@ -24,16 +25,16 @@ def submit():
                 Insulin= float(request.form["Insulin"])
                 BMI= float(request.form["BMI"])
                 DiabetesPedigreeFunction= float(request.form["DiabetesPedigreeFunction"])
-                Age= float(request.form["request"])
+                Age= float(request.form["Age"])
                 input_values = (Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age)
                 input_array = np.asarray(input_values)
                 input_array_reshaped = input_array.reshape(1,-1)
 
-                input_data_scaled = scaler.transform(input_array_reshaped)
+                input_data_scaled = scaler.transform(pd.DataFrame(input_array_reshaped,columns=["Pregnancies", "Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI", "DiabetesPedigreeFunction", "Age"]))
 
-                predicted_output = model.predict(input_data_scaled)
+                predicted_output = model.predict(input_data_scaled)[0]
 
-                return render_template("form.html",prediction = predicted_output)
+                return render_template("form.html",prediction = int(predicted_output))
             except Exception as e:
                  print(f"Unknown Error: {e}")
         
